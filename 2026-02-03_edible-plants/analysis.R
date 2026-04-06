a318135a4e26fbba13ceb46178ea81a23aabf4db
Zheng Yutong <yutongz507@gmail.com>
2026-04-06 23:03:09 +0800

    Update analysis.R

--- 2026-02-03_edible-plants/analysis.R
+++ 2026-02-03_edible-plants/analysis.R
@@ -1,16 +1,13 @@
 dfC <- select(df1, Name, Cultivation, Hardiness, minph = preferred_ph_lower, maxph = preferred_ph_upper)
 dfC$hardinessN <- as.numeric(dfC$Hardiness)
-table(dfC$hardinessN)
-table(dfC$Hardiness)
 dfC_summary <- dfC %>%
   group_by(Cultivation) %>%
   summarise(
-    minph = minph, 
-    maxph = maxph,
+    minph = min(minph, na.rm = TRUE),
+    maxph = max(maxph, na.rm = TRUE),
     medianH = median(hardinessN, na.rm = TRUE),
     .groups = "drop"
   )
-unique(dfC$Hardiness)
 ggplot(dfC_summary,
        aes(y = Cultivation,
            xmin = minph,
@@ -19,10 +16,9 @@ ggplot(dfC_summary,
   geom_linerange(size = 4, lineend = "round") +
   labs(colour = "Median Hardiness", y = "Cultivation Type", x = "pH Range")+
   scale_colour_discrete(drop = FALSE) + #check this
-  scale_color_manual(labels = c("Tender", "Medium", "Hardy", "Very Hardy"), 
-                     values = c("#dadaeb", "#9e9ac8", "#6a51a3", "#2d0a5e")) + #colours from colorbrewer
+  scale_color_manual(labels = c("Tender", "Medium", "Hardy", "Very Hardy"), values = c("#dadaeb", "#9e9ac8", "#6a51a3", "#2d0a5e")) + #colours from colorbrewer
 theme_minimal()+
   theme(
     panel.grid = element_blank(),      
-    axis.line = element_line(colour = "grey80") 
+    axis.line = element_line(colour = "black") 
   )
