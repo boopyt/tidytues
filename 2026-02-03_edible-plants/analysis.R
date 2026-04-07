6dea805b32da5e8a56329d98d4d2c26871278a08
Zheng Yutong <yutongz507@gmail.com>
2026-04-07 23:19:33 +0800

    Update analysis.R

--- 2026-02-03_edible-plants/analysis.R
+++ 2026-02-03_edible-plants/analysis.R
@@ -5,18 +5,27 @@ dfC_summary <- dfC %>%
   summarise(
     minph = min(minph, na.rm = TRUE),
     maxph = max(maxph, na.rm = TRUE),
-    medianH = median(hardinessN, na.rm = TRUE),
+    medianH = round(median(hardinessN, na.rm = TRUE)),
     .groups = "drop"
   )
+dfC_summary$medianH <- factor(dfC_summary$medianH,
+                              levels = c(1, 2, 3, 4, 5),
+                              labels = c("Very Tender", "Tender", "Medium", "Hardy", "Very Hardy")
+                              )
 ggplot(dfC_summary,
        aes(y = Cultivation,
            xmin = minph,
            xmax = maxph,
-           colour = factor(medianH))) +
+           colour = medianH)) +
   geom_linerange(size = 4, lineend = "round") +
   labs(colour = "Median Hardiness", y = "Cultivation Type", x = "pH Range")+
-  scale_colour_discrete(drop = FALSE) + #check this
-  scale_color_manual(labels = c("Tender", "Medium", "Hardy", "Very Hardy"), values = c("#dadaeb", "#9e9ac8", "#6a51a3", "#2d0a5e")) + #colours from colorbrewer
+  scale_color_manual(values = c("Very Tender" = "#f2f0f7",
+                                "Tender" = "#dadaeb",
+                                "Medium" = "#9e9ac8",
+                                "Hardy" = "#6a51a3",
+                                "Very Hardy" = "#2d0a5e"),
+                     drop = F)+ #colours from colorbrewer
+  scale_x_continuous(limits = c(4, 9), breaks = seq(4, 9, by = 1))+
 theme_minimal()+
   theme(
     panel.grid = element_blank(),      
